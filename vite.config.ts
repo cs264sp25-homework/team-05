0605e8d4b7757be80f44cbd70b0a3ba1f232c290
ahashi1263 <ahmedhashi1263@gmail.com>
2025-03-13 19:12:55 -0400

    Adding base url to the vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from "vite"
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/team-05/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
