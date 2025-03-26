4130015b0aa971e42b73e4cc0eeb4902f7bd41f1
enochappiah <eappiah4@jhu.edu>
2025-03-26 11:28:03 -0400

    trying the gapi client

--- convex/google.ts
+++ convex/google.ts
@@ -1,17 +1,41 @@
 'use node';
 import { v } from "convex/values";
-import { action, internalAction } from "./_generated/server";
+import { action } from "./_generated/server";
 import { google } from "googleapis";
-import { auth } from "./auth";
+import { loadAuth2, loadAuth2WithProps, loadClientAuth2, gapi } from 'gapi-script';
+
 
 
 export const listGoogleCalendarEvents = action({
   args: { accessToken: v.string(), code: v.string() },
   handler: async (ctx, args) => {    
     console.log("Access Token:", args.accessToken);
-    const auth = new google.auth.OAuth2();
+    const auth = new google.auth.OAuth2({
+      clientId: process.env.AUTH_GOOGLE_ID,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET,
+      
+    });
 
-    auth.setCredentials({ refresh_token: args.accessToken });
+    let gapiClient = loadClientAuth2(gapi, process.env.AUTH_GOOGLE_SECRET as string, "https://www.googleapis.com/auth/calendar https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile openid");
+    gapiClient.then((client) => {
+      
+    }
+
+    // auth.setCredentials({ 
+    //   refresh_token: args.accessToken,
+    //   access_token: args.accessToken,
+    //  });
+    // const auth = new google.auth.OAuth2({
+    //   clientOptions: {
+    //     clientId: process.env.AUTH_GOOGLE_ID,
+    //     clientSecret: process.env.AUTH_GOOGLE_SECRET,
+
+    //   },
+    //   scopes: ["https://www.googleapis.com/auth/calendar", "https://www.googleapis.com/auth/userinfo.email", "https://www.googleapis.com/auth/userinfo.profile", "openid"],
+      
+    // });
+
+    google.options({auth: auth});
 
     const calendar = google.calendar({ version: "v3", auth: auth });
     const res = await calendar.events.list({
