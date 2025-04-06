bb2398d68acaf065adb81211f7b4dda1705e7542
ahashi1263 <ahmedhashi1263@gmail.com>
2025-04-06 12:29:33 -0400

    Editing routes to get rid of additional team-05

--- src/store/router.ts
+++ src/store/router.ts
@@ -5,12 +5,12 @@ import { createRouter } from "@nanostores/router";
 const DEBUG = false;
 
 const pages = {
-  home: `${BASE_URL}/team-05/`, // Home page
-  chats: `${BASE_URL}/team-05/chats`, // Chats page
-  addChat: `${BASE_URL}/team-05/addChat`,
+  home: `${BASE_URL}`, // Home page
+  chats: `${BASE_URL}/chats`, // Chats page
+  addChat: `${BASE_URL}/addChat`,
   chat: `${BASE_URL}/chats/:chatId`, // View a specific chat
-  editChat: `${BASE_URL}/team-05/chats/:chatId/edit`, // Edit a specific chat
-  messages: `${BASE_URL}/team-05/chats/:chatId/messages`, // View all messages in a specific chat
+  editChat: `${BASE_URL}/chats/:chatId/edit`, // Edit a specific chat
+  messages: `${BASE_URL}/chats/:chatId/messages`, // View all messages in a specific chat
 
 };
 
